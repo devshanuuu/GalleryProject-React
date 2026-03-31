316d6b9af811b4b9eb6808f9381c7a5bf8b23bab
devshanuuu <shanuuu0207@gmail.com>
2026-04-01 02:04:07 +0530

    Made component of card

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import Cards from './components/Cards'
 
 const App = () => {
   const [UserData, setUserData] = useState([])
@@ -15,23 +16,17 @@ const App = () => {
     getData()
   }, [Index])  
 
-  let printUserData = <h3 className='text-gray-300 text-xs absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>Loading...</h3>
+  let printUserData = <h3 className='text-gray-300 text-xs absolute font-semibold top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>Loading...</h3>
   if(UserData.length>0) {
     printUserData = UserData.map(function(elem, idx){
       return <div key = {idx}>
-      <a href = {elem.url}>
-      <div className='h-40 w-44 overflow-hidden'>
-        <img className = 'h-full w-full object-cover'src = {elem.download_url} alt = ''/>
-      </div>
-      <h2 className='font-bold text-lg'>{elem.author}</h2>
-      </a>
+        <Cards elem = {elem} />
       </div>
     })
   }
   
   return (
     <div className='bg-black overflow-auto h-screen p-4 text-white'>
-      <h1 className='text-6xl'>{Index}</h1>
       <div className='flex flex-wrap gap-4'>{printUserData}</div>
       <div className='flex justify-center items-center gap-6 p-4'>
         <button className='bg-amber-400 text-sm text-black cursor-pointer active:scale-95 rounded px-4 py-2 font-semibold'
@@ -41,6 +36,7 @@ const App = () => {
              setUserData([])
             }
         }}>Prev</button>
+        <h4>Page {Index}</h4>
         <button className='bg-amber-400 text-sm text-black cursor-pointer active:scale-95 rounded px-4 py-2 font-semibold'
         onClick={() => {
           setIndex(Index+1)
