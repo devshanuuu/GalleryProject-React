81488b0b62805043a0c3470b4d862c129d94c2a6
devshanuuu <shanuuu0207@gmail.com>
2026-04-01 00:50:38 +0530

    Get data using API

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
 
 const App = () => {
-  function getData() {
-    console.log("Data aagya");
+  const [UserData, setUserData] = useState([])
+  
+  
+  const getData = async() => {
+    const response = await axios.get('https://picsum.photos/v2/list?page=3&limit=30')
+    setUserData(response.data)
     
   }
   
+  let printUserData = 'No user available'
+  if(UserData.length>0) {
+    printUserData = UserData.map(function(elem, idx){
+      return <div>
+      <div className='h-40 w-44 bg-white'>
+        <img className = 'h-full object-cover'src = {elem.download_url} alt = ''/>
+      </div>
+      <h2>{elem.author}</h2>
+      </div>
+    })
+  }
   return (
-    <div className='bg-black h-screen p-4 text-white'>
+    <div className='bg-black overflow-auto h-screen p-4 text-white'>
       <button onClick={getData}
-      className='bg-green-600 active:scale-95 mb-3 px-5 py-2 rounded text-white'>Get Data</button>
+      className='bg-green-600 active:scale-95 mb-3 px-5 py-2 rounded text-white'>Get Data
+      </button>
+
+      <div className='flex flex-wrap gap-4'>{printUserData}</div>
     </div>
   )
 }
